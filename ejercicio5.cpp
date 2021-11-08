dbc168404ca8e2550057e5bcf14f6a20fe3910fa
d-waldeck <daniela@buildonline.co>
2021-11-08 00:14:15 -0300

    ejercicio5

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -5,91 +5,50 @@
 
 using namespace std;
 
-void addToMinHeap(Graph* graph, int src, Heap* minHeap)
-{
-    ListNode* aux = graph->adyList[src];
-    while(aux)
-    {
-        cout << "adj a 0: " << aux->key << endl;
-        minHeap->add(aux->weight,src,aux->key);
-        aux = aux->next;
-    }
-}
-
 int* dijkstra(Graph* graph, int src)
 {
-    // int vCount = 1;
-    // int* distances = new int[graph->v];
-    // for (int i = 0; i < graph->v; i++)
-    // {
-    //     distances[i] = -1;
-    // }
-    // Heap* minHeap = new Heap(graph->v);
-
-    // addToMinHeap(graph, src, minHeap);
-    // while(!minHeap->isEmpty() && vCount <= graph->v)
-    // {
-    //     HeapNode* head = minHeap->poll();
-    //     vCount++;
-    //     if(head->to != src && distances[head->to] == -1)
-    //     {
-    //         distances[head->to] = distances[head->from] + head->cost;
-    //     }
-    //     addToMinHeap(graph, head->to, minHeap);
-    // }
-    
-
-    // return distances;
-
-    int* dist =  new int[graph->v];
-    int* prev =  new int[graph->v];
+    int* dist =  new int[graph->v+1];
     dist[src] = 0;
     Heap* heap = new Heap(graph->v);
-    for (int i = 0; i < graph->v; i++)
+    for (int i = 1; i <= graph->v; i++)
     {
         if(i != src)
         {
             dist[i] = INF;
-            prev[i] = -1;
         }
     }
 
     ListNode* v = graph->adyList[src];
     while(v)
     {
+        dist[v->key]=v->weight;
         heap->add(v->weight,src,v->key);
         v = v->next;
     }
 
     while(!heap->isEmpty())
     {
-        HeapNode* head = heap->poll();
-        dist[head->to] = dist[head->from] + head->cost;
 
+        HeapNode* head = heap->poll();
         ListNode* v = graph->adyList[head->to];
         while(v)
         {
             int alt = dist[head->to] + v->weight;
-            if(alt < dist[head->to])
+            if(alt < dist[v->key])
             {
-                dist[head->to] = alt;
-                prev[head->to] = head->from;
-                heap->add(v->weight,head->to,v->key);
+                dist[v->key] = alt;
+                heap->add(alt,head->to,v->key);
             }
             v = v->next;
         }
-            
     }
     return dist;
 
 }
 
 int main() {
-    cout << "Getting input" << endl;
     int V;
     cin >> V;
-
-    cout << "Initializing graph" << endl;
     Graph* graph = new Graph(V );
     int E;
     cin >> E;
@@ -106,12 +65,10 @@ int main() {
 
     int V2;
     cin >> V2;
-    for(int i = 1; i < V2; i++) {
-        cout << "Doing dijkstra for " << i << endl;
+    for(int i = 1; i <= V2; i++) {
         int* distances = dijkstra(graph, i);
-        cout << "Finished dijkstra for " << i << endl;
-        for(int j = 1; j < V2; j++) {
-            if(distances[j] == INF || j == i ){
+        for(int j = 1; j <= V2; j++) {
+            if(distances[j] == INF || j == i){
                 cout << -1 << endl;
             } else {
                 cout << distances[j] << endl;
