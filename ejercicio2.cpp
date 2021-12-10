66ae14ed6bd4aa4e62db7325a53bec5b05fd7681
d-waldeck <daniela@buildonline.co>
2021-12-10 14:34:20 -0300

    fix ejercicio2

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    Hash<int> *hash = *Hash<int> (N);
+    Hash<int> *hash = new Hash<int> (N);
     for(int i = 0; i < N; i++) {
         string word;
         cin >> word;
